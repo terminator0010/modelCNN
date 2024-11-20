02432b4bfbf1cae681ee4bfc27da76ba869726f2
terminator0010 <34940921+terminator0010@users.noreply.github.com>
2024-11-20 09:33:35 -0300

    Add files via upload

--- RNN_urban8k.py
+++ RNN_urban8k.py
@@ -23,3 +23,48 @@ sns.set()
 
 print('Versão tensorflow: ', tensorflow.__version__)
 
+metadata = pd.read_csv(r'C:\modelCNN-main\model\metadata\UrbanSound8K.csv')
+
+fsID = []
+classID = []
+occurID = []
+sliceID = []
+full_path = []
+
+for root, dirs, files in tqdm(os.walk('C:\modelCNN-main\model\audio\fold1')):
+    for file in files:
+        try:
+            fs = int(file.split('-')[0])
+            class_ =  int(file.split('-')[1])
+            occur = int(file.split('-')[2])
+            slice_ = file.split('-')[3]
+            slice_ = int(file.split('.')[0])
+            
+            fsID.append(fs)
+            classID.append(class_)
+            occurID.append(occur)
+            sliceID.append(slice_)
+            
+            full_path.append((root, file))
+        except ValueError:
+            continue    
+
+sound_list = ['ar_condicionado','buzina_de_carro','crianca_brincado','latido_de_cachorro','perfuracao','motor_em_marcha','']
+sound_dict = {em[0]:em[1] for em in enumerate(sound_list)}
+
+df = pd.DataFrame([fsID, classID, occurID, sliceID, full_path]).T
+print(df)
+
+df.columns = ['fsID', 'classID', 'occurID', 'sliceID', 'full_path']
+df['classID'] = df['classID'].map(sound_dict)
+print(df['classID'])
+
+df['path'] = df['full_path'].apply(lambda x: x[0] + '/' + x[1])
+
+print(df['path'])
+
+df['classID'].describe()
+df['classID'].value_counts()
+
+plt.figure(figsize=(18,7))
+sns.countplot(df['classID'])
